1804a6bffe389796ac9398797af2fa5fb1d00fdf
Stoychev <stoychev.nas@gmail.com>
2024-08-27 19:12:41 +0300

    Format code in Loading Overlay

--- components/LoadingOverlay.js
+++ components/LoadingOverlay.js
@@ -1,28 +1,28 @@
 import { Text, View, ActivityIndicator, StyleSheet } from 'react-native';
 
 function LoadingOverlay({ message, color }) {
-  return (
-    <View style={styles.rootContainer}>
-      <Text style={[styles.message, { color: color }]}>{message}</Text>
-      <ActivityIndicator size="large" />
-    </View>
-  );
+    return (
+        <View style={styles.rootContainer}>
+            <Text style={[styles.message, { color: color }]}>{message}</Text>
+            <ActivityIndicator size="large" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  rootContainer: {
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 32,
-    borderRadius: 10,
-    zIndex: 10
-  },
-  message: {
-    fontSize: 16,
-    marginBottom: 12,
-  },
+    rootContainer: {
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 32,
+        borderRadius: 10,
+        zIndex: 10
+    },
+    message: {
+        fontSize: 16,
+        marginBottom: 12,
+    },
 });
 
 export default LoadingOverlay;
